1ee93e7677ff4ec49ed1d09781affac52a0b3877
Tyler Green <tyler.green2@gmail.com>
2012-03-28 13:08:43 -0500

    added lisp directory

---
+++ lisp/.#borg.el
@@ -0,0 +1,1 @@
+jorge@famicom.local.64457
---
+++ lisp/borg.el
@@ -0,0 +1,6 @@
+; workflow
+; create new file (with annoying time format)
+; add jekyll header to file
+; create outline
+; M-x org-publish
+; jekyll --server
