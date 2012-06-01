3f1b5ed5d75a53416291e942ab291b89e444e90e
Tyler Green <tyler.green2@gmail.com>
2012-06-01 10:13:48 -0500

    updated borg utils

--- lisp/borg.el
+++ lisp/borg.el
@@ -7,28 +7,28 @@
 
 (defvar prog-blog-dir "~/blogs/tylergreen.github.com/_posts/")
 
-(defvar jekyll-post-meta-data 
+(defun jekyll-post-meta-data (title)
+  (format
 "
 ---
-layout: default
-title: 
+layout: post
+title: %s
 ---
-"
-)
+" title))
 
 (defun todays-post-file-name (post-title)
   (concat 
    prog-blog-dir
    (substring (shell-command-to-string "date +'%Y-%m-%d'") 0 -1)
-   "-" post-title
-   "-post.org"
+   "-" (replace-regexp-in-string " " "-" post-title)
+   "-post.textile"
   ))
 
 (defun new-post (post-title)
   (interactive "MName of Post? ") ;;prefix argument
   (let ((filename (todays-post-file-name post-title)))
 	(find-file filename)
-	(insert jekyll-post-meta-data)
+	(insert (jekyll-post-meta-data post-title))
 	))
 
 (provide 'borg)
